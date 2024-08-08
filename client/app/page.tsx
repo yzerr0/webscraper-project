19dd4b8b4a11cefb436eaaaae02aee40b84127a9
yz <youssef.zerroug1@gmail.com>
2024-08-08 00:11:52 -0700

    axios installation

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,19 @@
+'use client'
+
 import React, {useEffect, useState} from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 
 const Page = () => {
 
   useEffect(() => {
     fetch('http://localhost:3000/api/scrape').then(res => res.json()).then(data => console.log(data));
+    // axios("http://localhost:4000/api/scrape").then(res => console.log(res));
   });
 
   return (
     <div className="flex items-center justify-center">
-      <h1 className="flex">simple webscraper</h1>
-      <p className="flex">Enter a URL to scrape:</p>
+      <h1 className="block">simple webscraper</h1>
+      <p className="block">Enter a URL to scrape:</p>
     </div>
   );
 }
