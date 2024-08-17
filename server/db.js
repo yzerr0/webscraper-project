2f77c258d1c5f5e85b2ec297a473e7133d94cbe8
yz <youssef.zerroug1@gmail.com>
2024-08-16 23:35:20 -0700

    routes and authentication with user login and reg

---
+++ server/db.js
@@ -0,0 +1,22 @@
+const dummy = require("dummy.json");
+
+getUserById(id, callback) = () => {
+    try {
+        user = dummy.users.find(user => user.id === id);
+        callback(null, user);
+    } catch (error) {
+        callback(new Error("User not found"));
+    }
+}
+
+function createUser(user) {
+    return new Promise((resolve, reject) => {
+        const newUser = { id: dummy.users.length + 1, ...user }
+        dummy.users.push(newUser);
+        resolve(newUser);
+    });
+}
+
+module.exports = {
+    getUserById
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const cors = require("cors");
+const db = require("./db");
 
 const session = require("express-session");
 const passport = require("passport");
@@ -15,7 +16,6 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-/**
 app.use(
     session({
         secret: "djS*AD&a9dgh23",
@@ -25,21 +25,31 @@ app.use(
 );
 
 passport.use(
-    function(username, password, done) {
-    }
+    new LocalStrategy(function(username, password, done) {
+      if(username === "admin" && password === "admin") {
+          return done(null, { username: "admin" });
+      } else {
+          return done(null, false);
+      }
+  })
 )
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user);
 })
 
-passport.deserializeUser((user, done) => {
-
-})
+passport.deserializeUser((id, done) => {
+  db.getUserById(id, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, user);
+  });
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
-*/
+
 
 async function scraper(URL) {
   try {
@@ -70,6 +80,30 @@ app.get("/api/scrape/", async (req, res) => {
 
   const data = await scraper(URL);
   res.status(200).send(data);
+});
+
+app.post("/login",
+    passport.authenticate("local", { failureRedirect: "/login" }),
+    (req, res) => {
+        res.redirect("/profile");
+    }
+)
+
+app.post("/register", async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = await db.createUser({ username, password });
+        res.status(201).json({
+          msg: "User created successfully",
+          user,
+        });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile", { user: req.user });
 });
 
 app.use((err, req, res, next) => {
